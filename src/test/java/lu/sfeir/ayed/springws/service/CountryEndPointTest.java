e1fccb15cb669058887f0a33997e0ee78f4c0dc1
ayed.h <ayed.h@sfei.lu>
2016-02-04 11:01:29 +0100

    Adding integrations tests

---
+++ src/test/java/lu/sfeir/ayed/springws/service/CountryEndPointTest.java
@@ -0,0 +1,16 @@
+package lu.sfeir.ayed.springws.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ayed.h on 04/02/2016.
+ */
+public class CountryEndPointTest {
+
+    @Test
+    public void testGetCountry() throws Exception {
+
+    }
+}
---
+++ src/test/java/lu/sfeir/ayed/springws/service/WebConfigurationsTest.java
@@ -0,0 +1,7 @@
+package lu.sfeir.ayed.springws.service;
+
+/**
+ * Created by ayed.h on 04/02/2016.
+ */
+public class WebConfigurationsTest {
+}
